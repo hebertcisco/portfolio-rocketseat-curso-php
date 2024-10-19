4ab5ef9780d97d465403ac42bdcd110ddf845b5e
hebertcisco <hebertcisco@outlook.com>
2024-10-19 18:54:43 -0300

    Refactor home-page layout

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,4 +1,4 @@
-<main class="container mx-auto">
+<main>
     <livewire:components.sections.hero-section :user="$this->getUser()" />
     <livewire:components.sections.projects-section :user="$this->getUser()" />
     <livewire:components.sections.contact-section :user="$this->getUser()" />
